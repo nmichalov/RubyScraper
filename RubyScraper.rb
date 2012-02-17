dc6f4b8888b141427954a564ebf9b97416301694
Nathan Michalov <nmichalov@gmail.com>
2012-02-17 12:42:05 -0500

    Now the content of each page is saved to a numbered file, and the url associated with each text file is stored in a hash.

--- RubyScraper.rb
+++ RubyScraper.rb
@@ -3,12 +3,24 @@
 require 'anemone'
 require 'nokogiri'
 
-Anemone.crawl("http://www.hackerschool.com/") do |anemone|
-  anemone.on_every_page do |page|
-    @page = page.body
-    @doc = Nokogiri::HTML.parse(@page)
-    @doc.xpath('//p/text()').each do |p_tag|
-      File.open('test.txt', 'a') {|f| f.write(p_tag.content) }
+class DomainScraper
+  url_hash = Hash.new
+  count = 0
+  puts "Enter a domain to scrape:"
+  @domain = gets
+  Anemone.crawl(@domain) do |anemone|
+    anemone.on_every_page do |page|
+      @out_file = count.to_s()
+      url_hash[count] = page.url.to_s()
+      @page = page.body
+      @doc = Nokogiri::HTML.parse(@page)
+      @doc.xpath('//p/text()').each do |p_tag|
+        File.open(@out_file, 'a') {|f| f.write(p_tag.content) }
+      end
+      count += 1
     end
   end
+  puts url_hash
 end
+
+DomainScraper
